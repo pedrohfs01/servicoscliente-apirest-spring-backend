c620e51159049031b366108825a945692fe778ae
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-22 11:02:06 -0300

    Adicionar validações para serviços prestados

--- src/main/java/br/com/pedrohfs/servicoclientes/domain/dto/ServicoPrestadoDTO.java
+++ src/main/java/br/com/pedrohfs/servicoclientes/domain/dto/ServicoPrestadoDTO.java
@@ -4,12 +4,19 @@ package br.com.pedrohfs.servicoclientes.domain.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
 
+    @NotEmpty(message = "{campo.desc.obrigatorio}")
     private String descricao;
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 }
