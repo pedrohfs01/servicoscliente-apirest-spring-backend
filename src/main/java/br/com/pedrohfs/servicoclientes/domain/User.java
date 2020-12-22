3ff44e523df59c4edca3e81b990412ad8f8ab999
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-22 13:34:21 -0300

    Criar entidade usuário

---
+++ src/main/java/br/com/pedrohfs/servicoclientes/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.pedrohfs.servicoclientes.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, name = "login")
+    private String username;
+
+    @Column(name = "senha")
+    private String password;
+}
