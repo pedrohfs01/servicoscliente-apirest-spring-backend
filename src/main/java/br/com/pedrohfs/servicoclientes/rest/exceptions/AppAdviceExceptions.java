7e3a373e00285460d9f81da60b42cffbaea9946f
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-23 13:50:22 -0300

    Terminando registro

--- src/main/java/br/com/pedrohfs/servicoclientes/rest/exceptions/AppAdviceExceptions.java
+++ src/main/java/br/com/pedrohfs/servicoclientes/rest/exceptions/AppAdviceExceptions.java
@@ -28,7 +28,7 @@ public class AppAdviceExceptions {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity handleResponseStatusException(ResponseStatusException ex){
-        String mensagemErro = ex.getMessage();
+        String mensagemErro = ex.getReason();
         HttpStatus codigoStatus = ex.getStatus();
         ApiErrors apiErrors = new ApiErrors(mensagemErro);
         return new ResponseEntity(apiErrors, codigoStatus);
